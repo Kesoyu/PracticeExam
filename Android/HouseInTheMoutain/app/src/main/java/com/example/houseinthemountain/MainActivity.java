fbc6c2814c1e103cb989bf218c750144ca14fa93
Kesoyu <75736746+Kesoyu@users.noreply.github.com>
2023-01-17 13:56:07 +0100

    android house in the mountein procject

--- Android/HouseInTheMoutain/app/src/main/java/com/example/houseinthemountain/MainActivity.java
+++ Android/HouseInTheMoutain/app/src/main/java/com/example/houseinthemountain/MainActivity.java
@@ -3,12 +3,41 @@ package com.example.houseinthemountain;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView textView_likeNumber;
+    Button btn_like, btn_delete;
+    String likeTextAsset = " polubień";
+    int likeNumber = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn_like = findViewById(R.id.likeButton);
+        btn_delete = findViewById(R.id.deleteButton);
+        textView_likeNumber = findViewById((R.id.textView_likeNumber));
+        btn_like.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                likeNumber++;
+                updateLikeTextView();
+            }
+        });
+        btn_delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(likeNumber>0)
+                    likeNumber--;
+                updateLikeTextView();
+            }
+        });
+
     }
+    public void updateLikeTextView(){
+        textView_likeNumber.setText(likeNumber+likeTextAsset);
+    };
 }
