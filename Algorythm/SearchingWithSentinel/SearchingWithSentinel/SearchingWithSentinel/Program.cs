e54a8b4cbe1043c86c8eccd0b8790a67b89fcbdc
Kesoyu <75736746+Kesoyu@users.noreply.github.com>
2023-01-17 11:06:56 +0100

    Searching array with using a sentinel

---
+++ Algorythm/SearchingWithSentinel/SearchingWithSentinel/SearchingWithSentinel/Program.cs
@@ -0,0 +1,54 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+main();
+static void main()
+{
+    const int range = 50;
+    int[] array = new int[range];
+    Random random = new Random();
+    for(int i = 0; i < range; i++)
+        array[i] = random.Next(1, 10);
+    int result = searchingWithSentinel(array, 5);
+    if (result == -1)
+    {
+        Console.WriteLine("nie znaleziono szukanej wartości");
+    }
+    else
+    {
+        Console.WriteLine("Szukana wartośc znaduje się pod idx: " + result);
+    }
+}
+
+
+static int searchingWithSentinel(int[] array, int number)
+{
+    int[] coppied = new int[array.Length+1];
+    array.CopyTo(coppied, 0);
+    coppied[array.Length] = number;
+    showArray(coppied);
+    showArray(array);
+    for(int i = 0; i < coppied.Length; i++)
+    {
+        if (coppied[i] == number && i!=coppied.Length-1)
+        {
+            return i;
+        }
+        else if(coppied[i] == number && i == coppied.Length - 1)
+        {
+            return -1;
+        }
+    }
+    return -1;
+}
+
+static void showArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        if (i+1 != array.Length)
+            Console.Write(array[i] + ", ");
+        else
+            Console.Write(array[i] + "\n");
+    }
+
+}
