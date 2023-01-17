14bd3e53b6919f22eb7d02b4612e787d87e17fcf
Kesoyu <75736746+Kesoyu@users.noreply.github.com>
2023-01-17 14:49:24 +0100

    Person class ex

---
+++ Algorythm/ObjectProgrammingOsoba/ObjectProgrammingOsoba/ObjectProgrammingOsoba/Osoba.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectProgrammingOsoba
+{
+    internal class Osoba
+    {
+        private int id { get; set; }
+        private string name { get; set; }
+        public static int counter = 0;
+        public Osoba(int id, string name)
+        {
+            this.id = id;
+            this.name = name;
+            counter++;
+        }
+        public Osoba()
+        {
+            id = 0;
+            name = string.Empty;
+            counter++;
+        }
+        public Osoba(Osoba copy)
+        {
+            id = copy.id;
+            name = copy.name;
+            counter++;
+        }
+
+        public void Hi(string name)
+        {
+            Console.WriteLine("Cześć " + name + ", mam na imię " + this.name);
+        }
+
+        public void Hi()
+        {
+            Console.WriteLine("Brak Danych");
+        }
+
+    }
+}
---
+++ Algorythm/ObjectProgrammingOsoba/ObjectProgrammingOsoba/ObjectProgrammingOsoba/Program.cs
@@ -0,0 +1,26 @@
+﻿using ObjectProgrammingOsoba;
+
+Console.WriteLine("Witaj, Świat!");
+
+Console.WriteLine("Liczba zarejestrowanych osób to " + Osoba.counter);
+
+Osoba firstPerson = new Osoba();
+
+Console.WriteLine("Proszę wpisać swoje imię");
+string name = Console.ReadLine();//w przypadku pobrania wartości null przez metode ReadLine zostaje zwrócone string.Empty, co zapobiega ewentualnym błędą
+Console.WriteLine("Proszę wpisać swoje id");
+int id;
+while(!int.TryParse(Console.ReadLine(), out id))
+{
+    Console.WriteLine("wpisane Id musi być wartością numeryczną");
+}
+Osoba secondPerson = new Osoba(id, name);
+
+Osoba thirdPerson = new Osoba(secondPerson);
+
+firstPerson.Hi("Jan");
+secondPerson.Hi("Jan");
+thirdPerson.Hi("Jan");
+
+Console.WriteLine("Liczba zarejestrowanych osób to " + Osoba.counter);
+
