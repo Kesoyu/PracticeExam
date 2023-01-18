9e86190d10d6b55230b77601b3d0f33e08369970
Kesoyu <75736746+Kesoyu@users.noreply.github.com>
2023-01-18 02:28:41 +0100

    some trash changes

---
+++ WPF/CheckBoxBinding/CheckBoxBinding/CheckBoxBinding/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CheckBoxBinding
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Item> items = new List<Item>();
+        public MainWindow()
+        {
+            items.Add(new Item("first item"));
+            items.Add(new Item("second item"));
+            items.Add(new Item("third item"));
+            items.Add(new Item("fourth item"));
+            InitializeComponent();
+            
+            DataContext = this;
+
+        }
+    }
+}
